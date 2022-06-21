cc6e0c75ee4004f0972645d124327f05ed368068
Dan Gilleland <dagilleland@shaw.ca>
2022-06-21 10:04:17 -0600

    More tests to explain what's happening

--- examples/02-Simple-Static-Site-Tests/footer.test.ts
+++ examples/02-Simple-Static-Site-Tests/footer.test.ts
@@ -11,25 +11,36 @@ it('should work', (ctx) => {
 
 describe('footer content', () => {
     beforeEach(() => {
-        
+        global.document = document;
     })
 
     it('should have a document object by default because of the comment block at the top of this test file', () => {
         expect(document).toBeDefined();
     })
 
-    it('should be able to create a body element', () => {
-        // let body = document.createElement('body')
-        // expect(body).toBeDefined();
-        let created = document.querySelector('body');
-        expect(created).toBeDefined();
-        expect(created?.childNodes.length).toBe(0);
+    it('should have a body element in JSdom', () => {
+        let theBody = document.querySelector('body');
+        expect(theBody).not.toBeNull();
+    })
+
+    it('should not have an content in the document body in JSdom', () => {
+        let theBody = document.querySelector('body');
+        expect(theBody?.childNodes.length).toBe(0);
+    })
+
+    it('should allow appending to the body element', () => {
+        let theBody = document.querySelector('body');
+        let aHeader = document.createElement('header');
+        let lorem = document.createTextNode('Lorem Ipsum FTW');
+        aHeader.appendChild(lorem);
+        theBody?.appendChild(aHeader);
+        let actual = document.querySelector('header');
+        expect(actual).not.toBeNull();
+        expect(actual?.textContent).not.toBeFalsy();
     })
 
     it('should generate footer content', () => {
-        let body = document.createElement('body')
-        expect(body).toBeDefined();
-        document.appendChild(body);
+        // So, if all the above is passing, why can't my JavaScript module add to the document?
         footerDefault();
         expect(document.querySelector('footer')).toBeDefined();
     })
